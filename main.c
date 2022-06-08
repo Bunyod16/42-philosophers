3af2d9cb6c13dad0198b0b7685b4c0127d7dd518
Bunyodbek Shamsiddinov <bshamsid@u91z03s03.42kl.edu.my>
2022-06-08 19:13:27 +0800

    chore: added life function

--- main.c
+++ main.c
@@ -4,6 +4,7 @@
 #include "philo.h"
 #include <stdio.h>
 #include <sys/time.h>
+#include <stdlib.h>
 
 int init(int argc, char **argv, t_settings *settings)
 {
@@ -13,29 +14,56 @@ int init(int argc, char **argv, t_settings *settings)
     while (++i < argc)
         if (!ft_isnum(argv[i]))
             return (0);
-    settings->argc = argc - 1;
-    settings->philos = ft_atoi(argv[0]);        
-    settings->die_time = ft_atoi(argv[1]);
-    settings->eat_time = ft_atoi(argv[2]);
-    settings->sleep_time = ft_atoi(argv[3]);
+    settings->args = argc - 1;
+    settings->philo_num = ft_atoi(argv[1]);        
+    settings->die_time = ft_atoi(argv[2]);
+    settings->eat_time = ft_atoi(argv[3]);
+    settings->sleep_time = ft_atoi(argv[4]);
+    settings->philos = malloc(sizeof(t_philo) * settings->philo_num);
     if (argc == 6)
-        settings->stop_after = ft_atoi(argv[4]);
+        settings->stop_after = ft_atoi(argv[5]);
     return (1);
+}
+
+void    *life(void *philo)
+{
+    struct timeval current_time;
+    t_philo *p2;
+
+    p2 = (t_philo *)philo;
+    while (1)
+    {
+        gettimeofday(&current_time, NULL);
+        printf("%ld: Philo #%d says hi\n", current_time.tv_sec * 1000 + current_time.tv_usec/1000, p2->chair);
+        usleep(10000);
+    }
 }
 
 int main(int argc, char **argv)
 {
     t_settings  settings;
+    int         i;
+    pthread_t   *threads;
 
-    // struct timeval current_time;
-    // gettimeofday(&current_time, NULL);
-    // printf("%ld", current_time.tv_sec * 1000 + current_time.tv_usec/1000);
     if (argc < 5 || argc > 6 || !init(argc, argv, &settings))
     {
         printf("Error: Wrong arguements");
         return (0);
     }
-    // pthread_create(&newthread, NULL, myturn, NULL);
+    i = -1;
+    threads = malloc(sizeof(pthread_t) * settings.philo_num);
+    printf("%d num\n", settings.philo_num);
+    while (++i < settings.philo_num)
+    {
+        settings.philos[i].chair = i + 1;
+        pthread_create(&threads[i], NULL, life, (void *)&settings.philos[i]);
+    }
+    i = -1;
+    printf("test");
+    while (++i < settings.philo_num)
+    {
+        pthread_join(threads[i],NULL);
+    }
     // start threads for each philo
     // each philo display time 
     // each philo check status of next fork
--- philo.h
+++ philo.h
@@ -1,6 +1,17 @@
 #ifndef PHILO_H
 # define PHILO_H
 
+typedef struct s_philo
+{
+	int					chair;
+	long				eat_time;
+	int					holds_fork1;
+	int					holds_fork2;
+	int					meals;
+	pthread_mutex_t		fork; //need to destroy the mutex
+	pthread_t			death_tracker;
+}	t_philo;
+
 typedef struct s_settings
 {
 	int args;
@@ -12,18 +23,6 @@ typedef struct s_settings
 	t_philo *philos;
 }		t_settings;
 
-typedef struct s_philo
-{
-	int					chair;
-	long				eat_time;
-	int					holds_fork1;
-	int					holds_fork2;
-	int					meals;
-
-
-	pthread_mutex_t fork1; //need to destroy the mutex
-	pthread_mutex_t fork2; //need to destroy the mutex
-}	t_philo
 int	ft_atoi(const char *str);
 int ft_isnum(char *str);
 
